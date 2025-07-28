0f03815a35613f2311342906b8c265c565efdbbf
Giri <girimugan@gmail.com>
2025-07-28 00:57:06 -0400

    app use context update

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Reacttemp from './core-concepts/Reacttemp';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -11,8 +11,31 @@ import Welcome from './Welcome';
 import UserContext from './UserContext';
 import { ThemeContext } from './ThemeContext';
 import Dashboard from './Dashboard';
+import { StyleContext } from './StyleContext';
+import ComponentOne from './ComponentOne';
+import ComponentTwo from './ComponentTwo';
+import ComponentThree from './ComponentThree';
 
 function App() {
+
+  // const style = {
+  //   color: "red",
+  //   border: "2px solid blue",
+  //   backgroundColor: "Yellow",
+  //   padding: "10px",
+  //   margin: "10px",
+  // };
+
+  const [style, setStyle] = useState({
+
+    color: " #FF6F61",
+    backgroundColor: " #F7CAC9",
+    border: "2px solid: #92A8D1",
+    padding: "10px",
+    margin: "10px",
+  });
+
+
   return (
     <>
       {/* <Reacttemp /> */}
@@ -20,14 +43,23 @@ function App() {
       {/* <Demostate /> */}
       {/* <Usetable /> */}
       {/* <Welcome /> */}
-
+      {/* 
       <UserContext.Provider value={"Giri"}>
         <ThemeContext.Provider value={"red"}>
           <Dashboard />
-          </ThemeContext.Provider>
-      </UserContext.Provider>
+        </ThemeContext.Provider>
+      </UserContext.Provider> */}
+
+      <StyleContext.Provider value={{ style, setStyle }}>
+        <ComponentOne />
+        <ComponentTwo />
+        <ComponentThree />
+      </StyleContext.Provider>
+
+
     </>
   );
+
 }
 
 export default App;
